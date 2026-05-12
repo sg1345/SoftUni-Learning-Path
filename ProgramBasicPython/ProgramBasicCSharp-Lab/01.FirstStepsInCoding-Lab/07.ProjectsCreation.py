7598336b833338df1863103e03caf5b096501227
sg1345 <dimitkostz@gmail.com>
2026-05-12 15:21:21 +0200

    Add Solution: Project Creation
    A script designed to calculate the total workload for an architect based on a fixed hourly rate per project.

---
+++ ProgramBasicPython/ProgramBasicCSharp-Lab/01.FirstStepsInCoding-Lab/07.ProjectsCreation.py
@@ -0,0 +1,6 @@
+name = input()
+projects_count = int(input())
+
+hours_to_complete = projects_count * 3
+
+print(f'The architect {name} will need {hours_to_complete} hours to complete {projects_count} project/s.')

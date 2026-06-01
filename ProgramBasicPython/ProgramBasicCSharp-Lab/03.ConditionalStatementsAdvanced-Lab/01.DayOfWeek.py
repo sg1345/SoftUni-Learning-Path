5307943facd8d8f926cf1ae4e62b87f8652a0de9
sg1345 <dimitkostz@gmail.com>
2026-06-01 16:51:10 +0200

    Add Solution Day Of Week

---
+++ ProgramBasicPython/ProgramBasicCSharp-Lab/03.ConditionalStatementsAdvanced-Lab/01.DayOfWeek.py
@@ -0,0 +1,18 @@
+number_input = int(input())
+
+if number_input == 1:
+    print('Monday')
+elif number_input == 2:
+    print('Tuesday')
+elif number_input == 3:
+    print('Wednesday')
+elif number_input == 4:
+    print('Thursday')
+elif number_input == 5:
+    print('Friday')
+elif number_input == 6:
+    print('Saturday')
+elif number_input == 7:
+    print('Sunday')
+else:
+    print('Error')
